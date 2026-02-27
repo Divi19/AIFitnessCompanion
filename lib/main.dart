246dd757d078fcd38eb0116133ae41f9dc4ab0c9
aung0010 <qf00077@gmail.com>
2026-02-27 22:37:35 +0800

    Saved updates to main.dart and injury screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
+import 'injury_profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,11 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Firebase connected ✓'),
-        ),
-      ),
+      home: const InjuryProfileScreen(),
     );
   }
 }
